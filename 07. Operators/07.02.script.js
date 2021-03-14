0a04a2ea1ecfb204791b1a47836cfc27b4e265e0
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-03-14 18:15:31 +0530

    Add files via upload

---
+++ 07. Operators/07.02.script.js
@@ -0,0 +1,21 @@
+// external js file
+// Write all JavaScript code here
+
+// Arithmetic operators
+var num1 = 10;
+var num2 = 4;
+
+console.log('Addition ' + (num1 + num2));// 14
+console.log('Subtraction ' + (num1 - num2)); // 6
+console.log('Multiplication ' + num1 * num2); // 40
+console.log('Division ' + num1 / num2); // 2.5
+console.log('Modulus reminder ' + num1 % num2); // 2
+num1++
+console.log('after Increment ' + num1); // 11
+num2--; 
+console.log('after Decrement ' + num2); // 3
+
+
+num1 = 10;
+num2 = 4;
+console.log('Exponentiation ' + (num1 ** num2)); // (10 ** 4) = 10* 10 * 10 * 10 = 10000
---
+++ 07. Operators/07.06.script.js
@@ -0,0 +1,18 @@
+// external js file
+// Write all JavaScript code here
+
+// Conditional (? or ternary) Operator
+
+// age category
+var currentAge = 100;
+var category;
+
+category = (currentAge < 18) ? 'Minor' : 'Major';
+console.log('AGE category: ' + category);
+
+// fees category
+var isAuthorisedMember = true;
+var fees;
+
+fees = (isAuthorisedMember == true) ? 5 : 10;
+console.log('fees / charges: ' + fees); 
