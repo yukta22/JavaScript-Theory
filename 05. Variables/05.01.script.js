99ea5399407c5d424c9f026273bfec4364f95b01
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-03-14 18:14:13 +0530

    Add files via upload

---
+++ 05. Variables/05.01.script.js
@@ -0,0 +1,32 @@
+// external js file
+// Write all JavaScript code here
+
+// variables defined to hold different types of data
+var techName = 'JavaScript'; // String literal 
+var version = 6; // Number literal
+var isDone = true; // Boolean literal
+
+console.log('Learning '+techName+version);
+
+// ------------------------------
+
+// Declaring Variable
+var userName;
+
+// Assigning value
+userName = 'Saurabh';
+
+console.log('Welcome '+userName);
+
+// ------------------------------
+
+// Declaring multiple variables
+var firstName = 'Saurabh', lastName = 'saxena', age = 35, isMarried = 'true';
+
+// Declaring multiple variables in multiple lines for readability
+/* var firstName = 'Saurabh', 
+lastName = 'saxena', 
+age = 35, 
+isMarried = 'true'; */
+
+console.log('I am ' + firstName + ' ' + lastName);
